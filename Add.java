bc8d0a3c7bdc8047f1e9e35ff3e80028dbfc9473
Ankit <ankitprajapati3785@gmail.com>
2025-08-13 09:54:24 +0530

    file Added

---
+++ Add.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Add{
+  public static void main(String[] args){
+         Scanner sc=new Scanner(System.in);
+         System.out.println("Enter First Number");
+         int n1=sc.nextInt();
+         System.out.println("Enter Secand Number");
+         int n2=sc.nextInt();
+         int sum=n1+n2;
+         System.out.println("Sum of Two Number :"+sum);
+ }
+}
+
+         
