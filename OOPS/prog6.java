9bd88e8d40df819052bee8a2874bcc330c9851b6
Ankit <ankitprajapati3785@gmail.com>
2025-09-10 11:52:43 +0530

    OOPS practice

---
+++ OOPS/prog6.java
@@ -0,0 +1,16 @@
+package Class2;
+
+public class prog6 {
+    static class Vehicle{
+        static int fee=100;
+        final double regno = 123456;
+        String ownername;
+        String vehicletype;
+        Vehicle(String ownername,String vehicletype)
+
+        void updateregfee(int regfee){
+            System.out.println("the reg fee is updated to " fee+regfee);
+        }
+    }
+    
+}
